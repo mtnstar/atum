8259c80235413208570cbfa2f9662afab198bc2d
Janiss Binder <binder@puzzle.ch>
2016-04-09 11:23:33 +0200

    implemented uniquness for date scope on location

--- app/models/sunshine_time.rb
+++ app/models/sunshine_time.rb
@@ -1,6 +1,7 @@
 class SunshineTime < ApplicationRecord
   belongs_to :location
   before_save :default_values
+  validates_uniqueness_of :date, scope: [:location_id]
 
   def default_values
     self.minutes_per_hour ||= ''
